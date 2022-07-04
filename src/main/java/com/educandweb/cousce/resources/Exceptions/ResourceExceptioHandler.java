ef93a34f382e68b4a0dedf7bf692046ab6744b10
Luiz <luizcb1944@hotmail.com>
2022-07-04 09:11:48 -0300

    Exception handling update

--- src/main/java/com/educandweb/cousce/resources/Exceptions/ResourceExceptioHandler.java
+++ src/main/java/com/educandweb/cousce/resources/Exceptions/ResourceExceptioHandler.java
@@ -9,21 +9,31 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.educandweb.cousce.services.exceptions.DatabaseException;
 import com.educandweb.cousce.services.exceptions.ResourceNotFoundException;
 
 @ControllerAdvice
 public class ResourceExceptioHandler {
-	
-	
+
 	@ExceptionHandler(ResourceNotFoundException.class)
-	public ResponseEntity<StandardError>ResourceNotFound(ResourceNotFoundException e,HttpServletRequest request){
-		
+	public ResponseEntity<StandardError> ResourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
+
 		String error = "Resouce not found";
 		HttpStatus status = HttpStatus.NOT_FOUND;
-		StandardError err = new StandardError(Instant.now(),status.value(),error,e.getMessage(),request.getRequestURI());
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(),
+				request.getRequestURI());
 		return ResponseEntity.status(status).body(err);
-		
-		
+
 	}
 
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request) {
+
+		String error = "Database error";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(),
+				request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+
+	}
 }
--- src/main/java/com/educandweb/cousce/services/UserService.java
+++ src/main/java/com/educandweb/cousce/services/UserService.java
@@ -1,15 +1,19 @@
 package com.educandweb.cousce.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.educandweb.cousce.entities.User;
 import com.educandweb.cousce.repositories.UserRepository;
+import com.educandweb.cousce.services.exceptions.DatabaseException;
 import com.educandweb.cousce.services.exceptions.ResourceNotFoundException;
 
 import java.util.List;
 import java.util.Optional;
+
+import javax.persistence.EntityNotFoundException;
 
 
 @Service
@@ -36,12 +40,18 @@ public class UserService {
 			repository.deleteById(id);
 		}catch(EmptyResultDataAccessException e) {
 			throw new ResourceNotFoundException(id);
+		}catch(DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
 		}
 	}
 	public User update(Long id,User obj) {
+		try {
 		User entity = repository.getOne(id);
 		updateData(entity,obj);
 		return repository.save(entity);
+		}catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 	private void updateData(User entity, User obj) {
 		entity.setName(obj.getName());
